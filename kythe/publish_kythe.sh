aaa9acc69c1710afe2fb76d81b9fe6a4f5679b7a
Benjy Weinberger <benjyw@gmail.com>
2017-09-01 12:11:04 -0400

    A README for building Kythe releases.

--- kythe/publish_kythe.sh
+++ kythe/publish_kythe.sh
@@ -5,19 +5,7 @@
 # Since Kythe currently does not do regular releases, and in any case doesn't publish their
 # releases, we build and publish our own, under custom version numbers.
 #
-# To create a kythe release in ~/kythe_releases/ to publish:
-#
-# - Install Bazel from homebrew.
-# - Clone https://github.com/google/kythe
-# - in your clone:
-#   - `tools/modules/update.sh`
-#   - `bazel build kythe/release`
-#   - `tar xfz bazel-genfiles/kythe/release/kythe-vXX.YY.ZZ.tar.gz -C ~/kythe_releases/`
-#     where XX.YY.ZZ is the version of the most recent official Kythe release (0.0.26 at the time of writing).
-# - Give the release a custom version, e.g.,:
-# - `mv ~/kythe_releases/kythe-vXX.YY.ZZ ~/kythe_releases/kythe-vXX.YY.ZZ.5-snowchainNNN-HHHHHHHHHHH`
-#   Where NNN is some running count, and HHHHHHHHHHH is the prefix of the git sha in the Kythe repo
-#   at which we built the release (we currently take 11 digits).
+# See README.md for how to run this script.
 
 set -e
 
